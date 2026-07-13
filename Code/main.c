3f47d5eb5e30e265da6bb1708a7bb76cdf54c68a
Shivam022811 <shivam022811@gmail.com>
2026-07-13 11:21:12 +0530

    feat: integrate firmware modules into main application

--- Code/main.c
+++ Code/main.c
@@ -1,27 +1,50 @@
 /******************************************************************************
- * Project     : Embedded Contactless Digital Tachometer
- * File Name   : main.c
- * Author      : Shivam Kumar
- * Description : Main Application Entry Point
+ * Project : Embedded Contactless Digital Tachometer
+ * File    : main.c
+ * Author  : Shivam Kumar
+ * Target  : AT89S52
+ * Compiler: SDCC
  ******************************************************************************/
 
 #include "config.h"
 #include "delay.h"
 #include "lcd.h"
+#include "timer.h"
+#include "sensor.h"
+#include "rpm.h"
+#include "status.h"
 
-void System_Init(void);
+/******************************************************************************
+ * Initialize Complete System
+ ******************************************************************************/
+
+void System_Init(void)
+{
+    LCD_Init();
+
+    Timer0_Init();
+
+    Sensor_Init();
+}
 
 void main(void)
 {
     System_Init();
 
+    LCD_Clear();
+
+    LCD_String("Digital");
+
+    LCD_SetCursor(1,0);
+
+    LCD_String("Tachometer");
+
+    Delay_ms(2000);
+
+    LCD_Clear();
+
     while(1)
     {
 
     }
-}
-
-void System_Init(void)
-{
-    LCD_Init();
 }
